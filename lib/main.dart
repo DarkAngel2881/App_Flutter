2c865b61706b7609738fc5a7b4911b97ad9463a1
DarkAngel <matteo2881@gmail.com>
2023-06-30 16:49:35 +0200

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'pages/loading_page.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(),
+      home: LoadingPage(),
     );
   }
 }
