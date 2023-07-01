1bb967adae19d2267a5d30735079a076d6410591
DarkAngel <matteo2881@gmail.com>
2023-07-01 17:17:42 +0200

    Splash logo animation

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -11,16 +11,25 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   void initState() {
     super.initState();
-    //_goLoginPage();
+    _goLoginPage();
   }
 
-  int time = 20;
+  int time = 40;
+  double size = 180;
   _goLoginPage() async {
-    for (int i = time; i != 0; i--) {
-      await Future.delayed(const Duration(seconds: 1), () {});
-      setState(() {
-        time--;
-      });
+    for (int i = 0; i != 5; i++) {
+      for (time; time != 0; time--) {
+        await Future.delayed(const Duration(milliseconds: 5), () {});
+        setState(() {
+          size += 0.5;
+        });
+      }
+      for (time; time != 40; time++) {
+        await Future.delayed(const Duration(milliseconds: 5), () {});
+        setState(() {
+          size -= 0.5;
+        });
+      }
     }
 
     if (!mounted) return;
@@ -31,36 +40,40 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              // TODO Loading animation
-              'lib/images/vezlogo.png',
-              height: 100,
-            ),
-            /*Image.asset(
-              ''
-            ),*/
-            const SizedBox(
-              height: 50,
-              width: 50,
-            ),
-            Text(
-              // TODO Animation scale
-              'Bella Vez$time',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Image.asset(
+                    'lib/images/Vez ring.png',
+                    height: size,
+                  ),
+                  Image.asset(
+                    'lib/images/LogoInner.png',
+                    height: 190,
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+              const SizedBox(
+                height: 50,
+                width: 50,
+              ),
+              const Text(
+                // TODO Animation scale
+                'Bella Vez',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
