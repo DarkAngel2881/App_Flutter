8a6455955e89219252ad8fbe23c2c05b0426c956
DarkAngel <matteo2881@gmail.com>
2023-07-01 15:59:00 +0200

    Splash screen update (work in progress)

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:loginpage/pages/login_page.dart';
 
@@ -13,10 +11,10 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   void initState() {
     super.initState();
-    _goLoginPage();
+    //_goLoginPage();
   }
 
-  int time = 7;
+  int time = 20;
   _goLoginPage() async {
     for (int i = time; i != 0; i--) {
       await Future.delayed(const Duration(seconds: 1), () {});
@@ -33,16 +31,19 @@ class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               // TODO Loading animation
-              'lib/images/Vez-Logo.png',
-              height: 150,
+              'lib/images/vezlogo.png',
+              height: 100,
             ),
+            /*Image.asset(
+              ''
+            ),*/
             const SizedBox(
               height: 50,
               width: 50,
